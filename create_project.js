3210d8984ae331cf4065c939864640be1100ce92
Gemma <gemmawvoakes@gmail.com>
2017-06-07 18:00:43 +0100

    npm init and install now working and creating package.json

--- create_project.js
+++ create_project.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
 // const gitIgnore = require('./.gitIgnore')
 
 // check new project name doesn't already exist
@@ -16,41 +17,45 @@ const fs = require('fs');
 // - to check if a file already exists
 
 
-// if (!fs.stat('../new_project', function (err) {
-//     if (err) throw err;
-// })) {
-    fs.mkdir('../new_project', function (err) {
+
+fs.mkdir('../new_project', function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    exec('cd ../new_project && npm init -y && npm install -D mocha chai eslint', function (err) {
+    if (err) throw err;
+    });
+    fs.writeFile('../new_project/README.md', 'Hey, this is a new project!', function (err) {
+        if (err) throw err;
+        console.log('The README.md file has been saved!');
+    });
+    fs.writeFile('../new_project/index.js', 'w', function (err) {
+        if (err) throw err;
+        console.log('The index.js file has been saved!');
+    });
+    fs.readFile('./.gitignore_template', function (err, data) {
+        if (err) throw err;
+        fs.writeFile('../new_project/.gitignore', data, function (err) {
+            if (err) throw err;
+            console.log('The gitignore file has been saved!');
+        });
+    });
+    fs.mkdir('../new_project/spec', function (err) {
         if (err) {
             return console.error(err);
         }
-        fs.writeFile('../new_project/README.md', 'Hey, this is a new project!', function (err) {
+        fs.writeFile('../new_project/spec/index.spec.js', 'Hey, this is a new project!', function (err) {
             if (err) throw err;
-            console.log('The README.md file has been saved!');
+            console.log('The spec.js file has been saved!');
         });
-        fs.writeFile('../new_project/index.js', 'w', function (err) {
-            if (err) throw err;
-            console.log('The index.js file has been saved!');
-        });
-        fs.readFile('./.gitignore_template', function (err, data) {
-            if (err) throw err;
-            fs.writeFile('../new_project/.gitignore', data, function (err) {
-                if (err) throw err;
-                console.log('The gitignore file has been saved!');
-            });
-        });
-        fs.mkdir('../new_project/spec', function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            fs.writeFile('../new_project/spec/index.spec.js', 'Hey, this is a new project!', function (err) {
-                if (err) throw err;
-                console.log('The spec.js file has been saved!');
-            });
-            console.log('New spec directory created successfully!');
-        });
-        console.log('New project directory created successfully!');
+        console.log('New spec directory created successfully!');
     });
-// }
+    console.log('New project directory created successfully!');
+});
+
+
+
+
 
 
 // fs.open('<directory>', 'a+', (err, fd) => {
